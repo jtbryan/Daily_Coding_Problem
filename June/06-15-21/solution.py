7b4d2700416a27c7ada8bfb43ea4d7ecc21481eb
jtbryan <jonnib24@gmail.com>
2021-06-17 23:43:06 -0400

    Added 3 new challenges

--- June/06-15-21/solution.py
+++ June/06-15-21/solution.py
@@ -1,4 +1,11 @@
 def main():
+    '''
+    There are 4 ways of completing this problem (https://www.geeksforgeeks.org/find-the-smallest-positive-number-missing-from-an-unsorted-array/):
+        (1): Brute force - Iterating through to find all of the positive integers in the array, which at worst could take O(n^2)
+        (2): Sorting - Quick Sort would allow for in-place sorting with a time complexity of O(n log n) and a space complexity of O(1)
+        (3): Hash table or set - This would require building a new set of each found positive integer, would could result in a time complexity of O(n) but a space complexity of O(n)
+        (4): 
+    '''
     pass
 
 if __name__ == "__main__":
