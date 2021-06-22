d36960a9ffd7b5b187351e40fc1879e797d87f9d
jtbryan <jonnib24@gmail.com>
2021-06-21 22:48:16 -0400

    Finished problem 5

--- June/06-16-21/solution.py
+++ June/06-16-21/solution.py
@@ -1,4 +1,24 @@
+def car(pair):
+    ''' Return first element of pair '''
+    def return_first(a, b):
+        return a
+    return pair(return_first)
+
+def cdr(pair):
+    ''' Return last element of pair '''
+    def return_last(a, b):
+        return b
+    return pair(return_last)
+
+def cons(a, b):
+    ''' Constructs a pair '''
+    def pair(f):
+        return f(a, b)
+    return pair
+
 def main():
+    print(f"First: {car(cons(3,4))}")
+    print(f"Last: {cdr(cons(3,4))}")
     pass
 
 if __name__ == "__main__":
